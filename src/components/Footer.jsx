2e5f3b382dc41c85169550c6180c66612acc7508
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-01-08 21:53:54 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+     <div class="text-center mt-4 p-3 bg-light text-dark rounded">
+      <p class="mb-0">&copy; 2025 Mohammad Hafeez. All Rights Reserved.</p>
+    </div>
+    </div>
+  )
+}
+
+export default Footer

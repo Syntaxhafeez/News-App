21377baca7141d9b87492d5b9ed4e43eaf2a2392
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-01-08 22:26:33 +0530

    `Update JSX class names to use camelCase convention`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-     <div class="text-center mt-4 p-3 bg-light text-dark rounded">
-      <p class="mb-0">&copy; 2025 Mohammad Hafeez. All Rights Reserved.</p>
+     <div className="text-center mt-4 p-3 bg-light text-dark rounded">
+      <p className="mb-0">&copy; 2025 Mohammad Hafeez. All Rights Reserved.</p>
     </div>
     </div>
   )
